a74903edc154b136296bb767be2bb576004c3d2b
Joseph Redmond <josephredmond9121@gmail.com>
2021-02-27 12:47:52 -0800

    Add project files.

---
+++ AirConsoleLibrary/Models/ManifestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirConsoleLibrary.Models
+{
+    class ManifestModel
+    {
+        public List<GridSpotModel> SeatsAvailable { get; private set; }
+        public Dictionary<GridSpotModel, PassengerModel> SeatsTaken { get; set; }
+
+    }
+}
---
+++ AirConsoleLibrary/Models/PassengerModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirConsoleLibrary.Models
+{
+    class PassengerModel
+    {
+        public PassengerModel(string firstName, string lastName, string passportNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PassportNumber = passportNumber;
+        }
+        private string FirstName { get; }
+        private string LastName { get; }
+        private string FullName { 
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+        private string PassportNumber { get; }
+    }
+}
